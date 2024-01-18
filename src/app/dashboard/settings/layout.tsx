71ef5720c00122f8cc1f0c173c1f80f852489916
ror-webExpert <web.expert106@gmail.com>
2024-01-19 02:18:36 +0500

    my basic structure

---
+++ src/app/dashboard/settings/layout.tsx
@@ -0,0 +1,14 @@
+export default function SettingsLayout({
+children} : 
+{children: React.ReactNode
+})
+{
+    return(
+        <section>
+            <nav>
+                <h1> setting layout</h1>
+            </nav>
+            {children}
+        </section>
+    )
+}
