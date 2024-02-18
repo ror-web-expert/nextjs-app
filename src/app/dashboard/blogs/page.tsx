67c20ebc41e16be02ec59859b7b9be6e54820d27
ror-webExpert <web.expert106@gmail.com>
2024-02-19 01:31:11 +0500

    blog updated

--- src/app/dashboard/blogs/page.tsx
+++ src/app/dashboard/blogs/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Page(){
 return(
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <h1> Blog s</h1>
+    <h1> Blog</h1>
    </main>
 )
 }
